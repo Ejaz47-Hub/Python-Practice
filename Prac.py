cbc4f2e83c25b3f4d1f4afacae494e72dcb332c4
Ejaz47-Hub <ejaz47026@gmail.com>
2026-06-02 11:41:30 +0530

    New

--- Prac.py
+++ Prac.py
@@ -1,3 +1,18 @@
 # print("Hello World")
-name = input("Enter The Name :")
-print('The Name is',name)
+"""name = input("Enter The Name :")
+print('The Name is',name)"""
+
+
+'''def ejaz():
+    a = input("Enter The Name :")
+    a.strip().title()
+    print(f"Nigga {a}")
+ejaz()'''
+
+def calc():
+    x = int(input("Enter The Number For Square"))
+    print(f"The Square is {square(x)}")
+
+def square(n):
+    return n*n
+calc()
