e956234ae774d32333755262ae7d98a04187fb47
Ejaz47-Hub <ejaz47026@gmail.com>
2026-06-04 10:42:49 +0530

    Set1

---
+++ CS50-Set1/deep.py
@@ -0,0 +1,8 @@
+def Sensitive():
+    word = input("What is the Answers to the Great Question of Life, the universe, and Everything? ")
+
+    if word == "42" or word == 'forty-two' or word == 'forty two':
+        print('yes')
+    else :
+        print('no')
+Sensitive()
